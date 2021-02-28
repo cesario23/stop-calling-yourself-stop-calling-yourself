51d42456a98bce682bf9cd557fd7c0c4ab1df6ad
cesar juarez <cesar.juarez@codeimmersives.com>
2021-02-28 01:35:40 -0800

    my-code

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
 
+function sayHello (str){
+    return str + "???";
+}
+let result1 = sayHello ('hello')
+result1
+console.log (result1)
+
+function twostrings (str1, str2){
+    return str1 + " " + str2;
+}
+const result2 = twostrings ("oh weell", "hello");
+result2
+console.log (result2)
+
+function substraction (num1, num2, num3){
+    return num1 - num2 -num3;
+}
+const result3 = substraction (10, 1, 2);
+result3
+console.log (result3)
+
+function reverSubstraction (num1, num2, num3){
+    return num3 - num2 - num1;
+}
+const result4 = reverSubstraction (10, 1, 2)
+result4
+console.log (result4)
+
+function tripleString (str){
+    return str+str+str
+}
+const result5 = tripleString ("codeimmersives")
+result5
+console.log (result5)
+
+function squareFunction(num){
+    return  Math.sqrt(num)
+}
+const result6 = squareFunction(16);
+result6
+console.log (result6)
